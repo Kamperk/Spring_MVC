e81cbc5f7ce7b87b36790dcfd737227253bcbbf0
kapustinmaksim <kapustin-19-08@mail.ru>
2021-03-24 13:29:26 +0300

    OneToOne

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,35 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      Car car = new Car("Mazzeratti", 8192);
+      User userw = new User("Maxim","cherdak","user1@mail.ru" );
+      userw.setCar(car);
+      userService.add(userw);
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println("series_id = " + user.getCar());
+      }
+
+
+      context.close();
+   }
+}
